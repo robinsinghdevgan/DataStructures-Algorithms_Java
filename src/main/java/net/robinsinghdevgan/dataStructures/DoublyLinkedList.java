12be0a6dd835c4b73ece3f3ab50927983977bb43
Robin Singh <robinsinghdevgan@outlook.com>
2020-12-27 22:35:04 +0530

    DLL.java : implemented various List<E> methods

--- src/main/java/net/robinsinghdevgan/dataStructures/DoublyLinkedList.java
+++ src/main/java/net/robinsinghdevgan/dataStructures/DoublyLinkedList.java
@@ -4,67 +4,145 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.NoSuchElementException;
 
 public class DoublyLinkedList<E> implements List<E> {
+
+    private class Itr implements Iterator<E> {
+        private Node<E> lastReturned = null, next = null;
+        private int nextIndex;
+
+        Itr() {
+            next = first == null ? null : getNode(0);
+            nextIndex = 0;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return next == null;
+        }
+
+        @Override
+        public E next() {
+            if (!hasNext())
+                throw new NoSuchElementException();
+
+            lastReturned = next;
+            next = next.next;
+            nextIndex++;
+            return lastReturned.data;
+        }
+        
+    }
+    private class ListIter implements ListIterator<E> {
+        private Node<E> lastReturned = null, next = null;
+        private int nextIndex;
+
+        ListIter() {
+            next = first == null ? null : getNode(0);
+            nextIndex = 0;
+        }
+
+        ListIter(int index) {
+            next = index >= size ? null : getNode(index);
+            nextIndex = index;
+        }
+
+        @Override
+        public void add(E e) {
+            lastReturned = null;
+            if (next == null)
+                linkLast(e);
+            else
+                linkBefore(e, next);
+            nextIndex++;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return next == null;
+        }
+
+        @Override
+        public boolean hasPrevious() {
+            return nextIndex > 0;
+        }
+
+        @Override
+        public E next() {
+            if (!hasNext())
+                throw new NoSuchElementException();
+
+            lastReturned = next;
+            next = next.next;
+            nextIndex++;
+            return lastReturned.data;
+        }
+
+        @Override
+        public int nextIndex() {
+            return nextIndex;
+        }
+
+        @Override
+        public E previous() {
+            if (!hasPrevious())
+                throw new NoSuchElementException();
+            //check if we're at the last element
+            lastReturned = next = (next == null) ? last : next.prev;
+            nextIndex--;
+            return lastReturned.data;
+        }
+
+        @Override
+        public int previousIndex() {
+            return nextIndex - 1;
+        }
+
+        @Override
+        public void remove() {
+            if (lastReturned == null)
+                throw new IllegalStateException();
+
+            Node<E> lastNext = lastReturned.next;
+            unlink(lastReturned);
+            if (next == lastReturned)
+                next = lastNext;
+            else
+                nextIndex--;
+            lastReturned = null;
+        }
+
+        @Override
+        public void set(E e) {
+            if (lastReturned == null)
+                throw new IllegalStateException();
+            lastReturned.data = e;
+        }
+
+    }
+
     private static class Node<E> {
         Node<E> prev, next;
         E data;
 
-        Node(Node<E> prev, E data, Node<E> next){
-            this.data = data;
-            this.prev = prev;
-            this.next = next;
-        }
-
-        Node(E data){
+        Node(E data) {
             this.data = data;
             this.prev = null;
             this.next = null;
         }
+
+        Node(Node<E> prev, E data, Node<E> next) {
+            this.data = data;
+            this.prev = prev;
+            this.next = next;
+        }
     }
 
-    private class ListIter implements ListIterator<E> {
-        private Node<E> lastReturned = null, next = null;
-        private int nextIndex;
-        
-    }
+    private Node<E> first = null, last = null;
 
-    @Override
-    public int size() {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-    @Override
-    public boolean contains(Object o) {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-    @Override
-    public Iterator<E> iterator() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public Object[] toArray() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public <T> T[] toArray(T[] a) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
+    private int size = 0;
+    
     @Override
     public boolean add(E e) {
         // TODO Auto-generated method stub
@@ -72,15 +150,9 @@ public class DoublyLinkedList<E> implements List<E> {
     }
 
     @Override
-    public boolean remove(Object o) {
+    public void add(int index, E element) {
         // TODO Auto-generated method stub
-        return false;
-    }
 
-    @Override
-    public boolean containsAll(Collection<?> c) {
-        // TODO Auto-generated method stub
-        return false;
     }
 
     @Override
@@ -94,23 +166,29 @@ public class DoublyLinkedList<E> implements List<E> {
         // TODO Auto-generated method stub
         return false;
     }
-
-    @Override
-    public boolean removeAll(Collection<?> c) {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-    @Override
-    public boolean retainAll(Collection<?> c) {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
+    
     @Override
     public void clear() {
-        // TODO Auto-generated method stub
+        removeRange(0, size());
+    }
 
+
+    @Override
+    public boolean contains(Object o) {
+        @SuppressWarnings({ "unchecked" }) Node<E> n = (Node<E>) o;
+        Node<E> iterator = first;
+        while (iterator != null) {
+            if (n == iterator)
+                return true;
+            iterator = iterator.next;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        // TODO Auto-generated method stub
+        return false;
     }
 
     @Override
@@ -119,22 +197,20 @@ public class DoublyLinkedList<E> implements List<E> {
         return null;
     }
 
-    @Override
-    public E set(int index, E element) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+    Node<E> getNode(int index) {
+        // assert isElementIndex(index);
 
-    @Override
-    public void add(int index, E element) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public E remove(int index) {
-        // TODO Auto-generated method stub
-        return null;
+        if (index < (size >> 1)) {
+            Node<E> x = first;
+            for (int i = 0; i < index; i++)
+                x = x.next;
+            return x;
+        } else {
+            Node<E> x = last;
+            for (int i = size - 1; i > index; i--)
+                x = x.prev;
+            return x;
+        }
     }
 
     @Override
@@ -144,21 +220,115 @@ public class DoublyLinkedList<E> implements List<E> {
     }
 
     @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Itr();
+    }
+
+    @Override
     public int lastIndexOf(Object o) {
         // TODO Auto-generated method stub
         return 0;
     }
 
+    /**
+     * Inserts element e before non-null Node succ.
+     */
+    void linkBefore(E e, Node<E> succ) {
+        // assert succ != null;
+        final Node<E> pred = succ.prev;
+        final Node<E> newNode = new Node<>(pred, e, succ);
+        succ.prev = newNode;
+        if (pred == null)
+            first = newNode;
+        else
+            pred.next = newNode;
+        size++;
+    }
+
+    /**
+     * Links e as first element.
+     */
+    private void linkFirst(E e) {
+        final Node<E> f = first;
+        final Node<E> newNode = new Node<>(null, e, f);
+        first = newNode;
+        if (f == null)
+            last = newNode;
+        else
+            f.prev = newNode;
+        size++;
+    }
+
+    /**
+     * Links e as last element.
+     */
+    void linkLast(E e) {
+        final Node<E> l = last;
+        final Node<E> newNode = new Node<>(l, e, null);
+        last = newNode;
+        if (l == null)
+            first = newNode;
+        else
+            l.next = newNode;
+        size++;
+    }
+
     @Override
     public ListIterator<E> listIterator() {
+        return new ListIter();
+    }
+
+    @Override
+    public ListIterator<E> listIterator(int index) {
+        return new ListIter(index);
+    }
+
+    @Override
+    public E remove(int index) {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
-    public ListIterator<E> listIterator(int index) {
+    public boolean remove(Object o) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    protected void removeRange(int fromIndex, int toIndex) {
+        ListIterator<E> it = listIterator(fromIndex);
+        for (int i=0, n=toIndex-fromIndex; i<n; i++) {
+            it.next();
+            it.remove();
+        }
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public E set(int index, E element) {
         // TODO Auto-generated method stub
         return null;
+    }
+
+    @Override
+    public int size() {
+        return size;
     }
 
     @Override
@@ -166,5 +336,61 @@ public class DoublyLinkedList<E> implements List<E> {
         // TODO Auto-generated method stub
         return null;
     }
-    
+
+    @Override
+    public Object[] toArray() {
+        Object[] array = new Object[size];
+        int index = 0;
+        Node<E> iterator = first;
+        while (iterator != null) {
+            array[index++] = iterator.data;
+            iterator = iterator.next;
+        }
+        return array;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T> T[] toArray(T[] a) {
+        if (a.length < size)
+        a = (T[])java.lang.reflect.Array.newInstance(a.getClass().getComponentType(), size);
+        int i = 0;
+        Object[] result = a;
+        for (Node<E> x = first; x != null; x = x.next)
+            result[i++] = x.data;
+
+        if (a.length > size)
+            a[size] = null;
+
+        return a;
+    }
+
+    /**
+     * Unlinks non-null node x.
+     */
+    E unlink(Node<E> x) {
+        // assert x != null;
+        final E element = x.data;
+        final Node<E> next = x.next;
+        final Node<E> prev = x.prev;
+
+        if (prev == null) {
+            first = next;
+        } else {
+            prev.next = next;
+            x.prev = null;
+        }
+
+        if (next == null) {
+            last = prev;
+        } else {
+            next.prev = prev;
+            x.next = null;
+        }
+
+        x.data = null;
+        size--;
+        return element;
+    }
+
 }
