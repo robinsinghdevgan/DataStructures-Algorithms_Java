74464bcb609fa5b7eb9433991527f1b86ffea2c6
Robin Singh <robinsinghdevgan@outlook.com>
2020-12-27 13:52:17 +0530

    Added more tests

--- src/test/java/net/robinsinghdevgan/dataStructures/SinglyLinkedListTest.java
+++ src/test/java/net/robinsinghdevgan/dataStructures/SinglyLinkedListTest.java
@@ -2,30 +2,53 @@ package net.robinsinghdevgan.dataStructures;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class SinglyLinkedListTest {
 
-    SinglyLinkedList<Integer> sll = null;
+    final int[] arr = new int[]{0,1,2,3,4,5,6,7};
+        
     @org.junit.jupiter.api.BeforeAll
     void setUp() {
-        var arr = new int[]{0,1,2,3,4,5,6,7};
-        sll = new SinglyLinkedList<Integer>();
-        for (int i:arr){
-            sll.add(i);
-        }
-        System.out.println("=========List setup=====");
-        System.out.println(sll.toString());
     }
 
     @org.junit.jupiter.api.AfterAll
     void tearDown() {
-        sll = null;
     }
     
     @org.junit.jupiter.api.Test
+    SinglyLinkedList<Integer> testCollectionToList() {
+        SinglyLinkedList<Integer> sll = null;
+        sll = new SinglyLinkedList<Integer>();
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i:arr){
+            list.add(i);
+        }
+        assertTrue(sll.addAll(list));
+        //System.out.println("=========List setup=====");
+        //System.out.println(sll.toString());
+        return sll;
+    }
+
+    @Test
+    void removeItem() {
+        SinglyLinkedList<Integer> sll = null;
+        sll = testCollectionToList();
+        sll.remove(0);
+        assertEquals("1 => 2 => 3 => 4 => 5 => 6 => 7 => ", sll.toString());
+        sll.remove(6);
+        assertEquals("1 => 2 => 3 => 4 => 5 => 6 => ", sll.toString());
+        sll.remove(3);
+        assertEquals("1 => 2 => 3 => 5 => 6 => ", sll.toString());
+    }
+
+    @org.junit.jupiter.api.Test()
     void getItem() {
+        final SinglyLinkedList<Integer> sll = testCollectionToList();
         for(int i = 0; i < 8; i++) {
             System.out.println("i: " + i + " -> " + sll.get(i));
             assertEquals(i, sll.get(i));
