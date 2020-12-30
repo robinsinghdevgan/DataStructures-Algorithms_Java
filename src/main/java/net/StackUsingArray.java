17adddad84b694a6bffe2143150ba77acc5c5e94
Robin Singh <robinsinghdevgan@outlook.com>
2020-12-30 19:10:35 +0530

    Implemented Stack using arrays

---
+++ src/main/java/net/StackUsingArray.java
@@ -0,0 +1,71 @@
+package net;
+
+import java.util.EmptyStackException;
+
+public class StackUsingArray<E> {
+    private int size = 0;
+    private int top = -1;
+    private Object[] array = null;
+
+    public StackUsingArray(int size) {
+        this.size = size;
+        array = new Object[size];
+    }
+
+    public StackUsingArray() {
+        size = 15;
+        array = new Object[size];
+    }
+
+    public E push(E item) throws IndexOutOfBoundsException{
+        try{
+            array[++top] = item;
+        }catch (IndexOutOfBoundsException ex){
+            throw new StackOverflowError(ex.toString());
+        }
+        return item;
+    }
+
+    public boolean isFull() {
+        return top == size-1;
+    }
+
+    public E pop() {
+        if(this.isEmpty())
+            throw new EmptyStackException();
+        return element(top--);
+    }
+
+    public E peek() {
+        if(this.isEmpty())
+            throw new EmptyStackException();
+        return element(top);
+    }
+
+    // Safe because push(T) is type checked.
+    @SuppressWarnings("unchecked")
+    private E element(int index) {
+        return (E)array[index];
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getTop() {
+        return top;
+    }
+
+    public void setTop(int top) {
+        this.top = top;
+    }
+    
+}
