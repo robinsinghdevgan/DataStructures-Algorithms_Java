5faf33e40591e2b67cc6ff18ad885a4fb75d4469
Robin Singh <robinsinghdevgan@outlook.com>
2020-12-30 19:10:54 +0530

    Added tests for StackUsingArray

---
+++ src/test/java/net/robinsinghdevgan/dataStructures/StackUsingArrayTest.java
@@ -0,0 +1,58 @@
+package net.robinsinghdevgan.dataStructures;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.EmptyStackException;
+
+import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import net.StackUsingArray;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class StackUsingArrayTest {
+
+    private StackUsingArray<String> s;
+    private final int size = 15;
+
+    @BeforeAll
+    public void setup() {
+        s = new StackUsingArray<String>(size);
+    }
+
+    @Test
+    public void test1() {
+        assertEquals(true, s.isEmpty());
+        assertEquals(15, s.getSize());
+        s.push("a");
+        assertEquals(false, s.isEmpty());
+        assertEquals("a", s.peek());
+        assertEquals("a", s.pop());
+        assertEquals(true, s.isEmpty());
+    }
+
+    @Test
+    public void test2() {
+        String[] x = { "a", "b", "c", "d" };
+        for (int i = 0; i < x.length; i++) {
+            s.push(x[i]);
+            assertEquals(size, s.getSize());
+            assertEquals(x[i], s.peek());
+        }
+
+        for (int i = x.length - 1; i >= 0; i--) {
+            assertEquals(x[i], s.pop());
+        }
+        assertEquals(true, s.isEmpty());
+    }
+
+    @Test()
+    public void test3() {
+        Assertions.assertThrows(EmptyStackException.class, () -> {
+            s.pop();
+        });
+    }
+}
