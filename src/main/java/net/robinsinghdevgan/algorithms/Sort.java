fb41dc1ab60218d7ba0a303f411f516d931bff48
Robin Singh <robinsinghdevgan@outlook.com>
2021-01-03 20:07:39 +0530

    Added bubble and insertion sort

---
+++ src/main/java/net/robinsinghdevgan/algorithms/Sort.java
@@ -0,0 +1,37 @@
+package net.robinsinghdevgan.algorithms;
+
+public class Sort {
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void doBubbleSort(int[] arr) {
+        int size = arr.length;
+        for (int i = 0; i < size; ++i) {
+            int current = arr[i];
+            for (int j = i + 1; j < size; ++j) {
+                if (current > arr[j])
+                    swap(arr, i, j);
+            }
+        }
+    }
+
+    public static void doInsertionSort(int[] arr) {
+        int size = arr.length;
+        //sortedPartitionIndex starts from 1 as 0th is already a sorted element.
+        for (int sortedPartitionIndex = 1; sortedPartitionIndex < size; sortedPartitionIndex++) {
+            int current = arr[sortedPartitionIndex];
+            //index i runs backwards, starts from sortedPartitionIndex - 1
+            int i = sortedPartitionIndex - 1;
+            while(i > -1 && arr[i] > current) {
+                arr[i+1] = arr[i];
+                --i;
+            }
+            // (i+1)th is the correct Index for 'current' element
+            //finally make the (i+1)th value as arr[sortedPartitionIndex]
+            arr[i+1] = current;
+        }
+    }
+}
---
+++ src/test/java/net/robinsinghdevgan/algorithms/SortingTest.java
@@ -0,0 +1,35 @@
+package net.robinsinghdevgan.algorithms;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class SortingTest {
+    int[] arr1 = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 100, 200, 50};
+    int[] arrSorted = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 50, 100, 200};
+    int[] arrUnsorted = null;
+    @BeforeAll
+    public void setUp() {
+        arrUnsorted = arr1.clone();
+    }
+
+    @Test
+    public void testBubbleSort() {
+        Sort.doBubbleSort(arrUnsorted);
+        //Arrays.sort(arrUnsorted);
+        assertThat(arrUnsorted).isEqualTo(arrSorted);
+        assertThat(arrUnsorted).isNotEqualTo(arr1);
+    }
+
+    @Test
+    public void testInsertionSort() {
+        Sort.doInsertionSort(arrUnsorted);
+        assertThat(arrUnsorted).isEqualTo(arrSorted);
+        assertThat(arrUnsorted).isNotEqualTo(arr1);
+    }
+}
