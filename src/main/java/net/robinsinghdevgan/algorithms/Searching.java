42a0768dcd76f3981cbe54659ef87650751a45df
Robin Singh <robinsinghdevgan@outlook.com>
2021-01-04 19:22:05 +0530

    Added binary and linear search

---
+++ src/main/java/net/robinsinghdevgan/algorithms/Searching.java
@@ -0,0 +1,27 @@
+package net.robinsinghdevgan.algorithms;
+
+public class Searching {
+    public static int linearSearch(int[] arr, int key) {
+        for(int i = 0; i < arr.length; i++) {
+            if(key == arr[i])
+                return i;
+        }
+        return -1;
+    }
+
+    public static int binarySearch(int[] arr, int key) {
+        int start = 0, end = arr.length - 1;
+        
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            if(key == arr[mid])
+                return mid;
+            else if(key < arr[mid]){
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/test/java/net/robinsinghdevgan/algorithms/SearchingTest.java
@@ -0,0 +1,27 @@
+package net.robinsinghdevgan.algorithms;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class SearchingTest {
+    final int[] arrUnsorted = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 100, 200, 50 };
+    final int[] arrSorted = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 50, 100, 200 };
+
+    @Test
+    public void testLinearSearch() {
+        assertThat(Searching.linearSearch(arrUnsorted, 50)).isEqualTo(12);
+        for(int i = 0; i<arrSorted.length; i++) {
+            assertThat(Searching.linearSearch(arrSorted, arrSorted[i])).isEqualTo(i);
+        }
+    }
+
+    @Test
+    public void testBinarySearch() {
+        for(int i = 0; i<arrSorted.length; i++) {
+            assertThat(Searching.binarySearch(arrSorted, arrSorted[i])).isEqualTo(i);
+        }
+    }
+}
