3c8d4e8ba97c5f1bf5b308034ca6c8e465a1308a
Robin Singh <robinsinghdevgan@outlook.com>
2021-01-07 12:44:11 +0530

    + get set methods

--- src/main/java/net/robinsinghdevgan/dataStructures/Graphs/Edge.java
+++ src/main/java/net/robinsinghdevgan/dataStructures/Graphs/Edge.java
@@ -1,10 +1,10 @@
 package net.robinsinghdevgan.dataStructures.Graphs;
 
-public class Edge <VertexDataType extends Comparable<VertexDataType>>{
-    VertexDataType from, to;
+public class Edge <E extends Comparable<E>>{
+    E from, to;
     Double cost;
 
-    public Edge(VertexDataType from, VertexDataType to, Double cost) {
+    public Edge(E from, E to, Double cost) {
         this.from = from;
         this.to = to;
         this.cost = cost;
@@ -13,5 +13,29 @@ public class Edge <VertexDataType extends Comparable<VertexDataType>>{
     @Override
     public String toString() {
         return "Edge{To: " + to + ", Cost: " + cost + "}";
+    }
+
+    public E getFrom() {
+        return from;
+    }
+
+    public void setFrom(E from) {
+        this.from = from;
+    }
+
+    public E getTo() {
+        return to;
+    }
+
+    public void setTo(E to) {
+        this.to = to;
+    }
+
+    public Double getCost() {
+        return cost;
+    }
+
+    public void setCost(Double cost) {
+        this.cost = cost;
     }
 }
