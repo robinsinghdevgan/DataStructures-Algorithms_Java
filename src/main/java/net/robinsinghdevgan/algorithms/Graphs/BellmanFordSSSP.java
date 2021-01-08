bfabb5212ec00f528bb24838a4709b81f824d0fd
Robin Singh <robinsinghdevgan@outlook.com>
2021-01-08 14:37:21 +0530

    + Bellman Ford SSSP

---
+++ src/main/java/net/robinsinghdevgan/algorithms/Graphs/BellmanFordSSSP.java
@@ -0,0 +1,64 @@
+package net.robinsinghdevgan.algorithms.Graphs;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+import net.robinsinghdevgan.dataStructures.Graphs.Edge;
+import net.robinsinghdevgan.dataStructures.Graphs.Graph;
+
+public class BellmanFordSSSP<E extends Comparable<E>> {
+
+    private List<String> reconstructPath(HashMap<E, Double> distance, HashMap<E, E> prev, E dest) {
+        List<String> path = new ArrayList<>();
+        if (distance.get(dest) == 0.0 || distance.get(dest) == Double.NEGATIVE_INFINITY)
+            return path;
+        var at = prev.get(dest);
+        while (at != null) {
+            path.add(at.toString());
+            at = prev.get(at);
+        }
+        Collections.reverse(path);
+        return path;
+    }
+
+    public HashMap<E, String> getShortestPath(Graph<E> graph, E source) {
+        int V = graph.numberOfVertices();
+        int iterations = V - 1;
+
+        var distCost = new HashMap<E, Double>();
+        var prev = new HashMap<E, E>();
+        for (var vertex : graph.vertices()) {
+            distCost.put(vertex.getData(), Double.POSITIVE_INFINITY);
+        }
+        distCost.put(source, 0.0);
+
+        // For each vertex, apply relaxation for all the edges
+        for (int i = 0; i < iterations; i++)
+            for (Edge<E> edge : graph.getEdges())
+                if (distCost.get(edge.getFrom()) + edge.getCost() < distCost.get(edge.getTo())) {
+                    distCost.put(edge.getTo(), distCost.get(edge.getFrom()) + edge.getCost());
+                    prev.put(edge.getTo(), edge.getFrom());
+                }
+
+        // Run algorithm a second time to detect which nodes are part
+        // of a negative cycle. A negative cycle has occurred if we
+        // can find a better path beyond the optimal solution.
+        for (int i = 0; i < iterations; i++)
+            for (Edge<E> edge : graph.getEdges())
+                if (distCost.get(edge.getFrom()) + edge.getCost() < distCost.get(edge.getTo())) {
+                    distCost.put(edge.getTo(), Double.NEGATIVE_INFINITY);
+                    prev.put(edge.getTo(), edge.getFrom());
+                }
+
+        // Return the array containing the shortest distance to every node
+        var result = new HashMap<E, String>();
+        for (var dist : distCost.entrySet()) {
+            result.put(dist.getKey(),
+                    reconstructPath(distCost, prev, dist.getKey()) + " and Costs: " + dist.getValue().toString());
+        }
+        return result;
+    }
+
+}
---
+++ src/test/java/net/robinsinghdevgan/algorithms/Graphs/BellmanFordSSSPTest.java
@@ -0,0 +1,109 @@
+package net.robinsinghdevgan.algorithms.Graphs;
+
+import java.util.HashMap;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import net.robinsinghdevgan.dataStructures.Graphs.Graph;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class BellmanFordSSSPTest {
+    private Graph<Integer> createGraphWithOnlyPositiveEdgeCosts() {
+        var graph = new Graph<Integer>();
+        for (int i = 0; i < 9; i++) {
+            graph.addVertex(i);
+        }
+        graph.addEdge(0, 1, 4.0);
+        graph.addEdge(0, 7, 8.0);
+        graph.addEdge(1, 2, 8.0);
+        graph.addEdge(1, 7, 1.0);
+        graph.addEdge(2, 3, 7.0);
+        graph.addEdge(2, 8, 2.0);
+        graph.addEdge(2, 5, 4.0);
+        graph.addEdge(3, 4, 9.0);
+        graph.addEdge(3, 5, 1.0);
+        graph.addEdge(4, 5, 1.0);
+        graph.addEdge(5, 6, 2.0);
+        graph.addEdge(6, 7, 1.0);
+        graph.addEdge(6, 8, 6.0);
+        graph.addEdge(7, 8, 7.0);
+        return graph;
+    }
+
+    private Graph<Integer> createGraphWithOnlyNegativeEdgeCosts() {
+        var graph = new Graph<Integer>();
+        for (int i = 0; i < 9; i++) {
+            graph.addVertex(i);
+        }
+        graph.addEdge(0, 1, 1.0);
+        graph.addEdge(1, 2, 1.0);
+        graph.addEdge(2, 4, 1.0);
+        graph.addEdge(4, 3, -3.0);
+        graph.addEdge(3, 2, 1.0);
+        graph.addEdge(1, 5, 4.0);
+        graph.addEdge(1, 6, 4.0);
+        graph.addEdge(5, 6, 5.0);
+        graph.addEdge(6, 7, 4.0);
+        graph.addEdge(5, 7, 3.0);
+        return graph;
+    }
+
+    @Test
+    public void testGraphWithOnlyPositiveEdgeCosts() {
+        var graph1 = createGraphWithOnlyPositiveEdgeCosts();
+        System.out.println(graph1.toString());
+
+        var checkMap = new HashMap<Integer, String>();
+        checkMap.put(0, "[] and Costs: 0.0");
+        checkMap.put(1, "[0] and Costs: 4.0");
+        checkMap.put(2, "[0, 1] and Costs: 12.0");
+        checkMap.put(3, "[0, 1, 2] and Costs: 19.0");
+        checkMap.put(4, "[0, 1, 2, 3] and Costs: 28.0");
+        checkMap.put(5, "[0, 1, 2] and Costs: 16.0");
+        checkMap.put(6, "[0, 1, 2, 5] and Costs: 18.0");
+        checkMap.put(7, "[0, 1] and Costs: 5.0");
+        checkMap.put(8, "[0, 1, 7] and Costs: 12.0");
+
+        Integer sourceVertex = 0;
+        var obj = new BellmanFordSSSP<Integer>();
+        var distanceMap = obj.getShortestPath(graph1, sourceVertex);
+        assertThat(distanceMap).isEqualTo(checkMap);
+
+        for (var entry : distanceMap.entrySet()) {
+            System.out.println("Path from: '" + sourceVertex + "' to: " + entry.getKey() + " : " + entry.getValue());
+        }
+    }
+
+    @Test
+    public void testGraphWithOnlyNegativeEdgeCosts() {
+        var graph1 = createGraphWithOnlyNegativeEdgeCosts();
+        System.out.println(graph1.toString());
+
+        Integer sourceVertex = 0;
+        var obj = new BellmanFordSSSP<Integer>();
+        var distanceMap = obj.getShortestPath(graph1, sourceVertex);
+
+        System.out.println("Null path with Infinite cost represents a negative Cycle.");
+        for (var entry : distanceMap.entrySet()) {
+            System.out.println(
+                    "Shortest Path from: '" + sourceVertex + "' to: " + entry.getKey() + " : " + entry.getValue());
+        }
+
+        var checkMap = new HashMap<Integer, String>();
+        checkMap.put(0, "[] and Costs: 0.0");
+        checkMap.put(1, "[0] and Costs: 1.0");
+        checkMap.put(2, "[] and Costs: -Infinity");
+        checkMap.put(3, "[] and Costs: -Infinity");
+        checkMap.put(4, "[] and Costs: -Infinity");
+        checkMap.put(5, "[0, 1] and Costs: 5.0");
+        checkMap.put(6, "[0, 1] and Costs: 5.0");
+        checkMap.put(7, "[0, 1, 5] and Costs: 8.0");
+        checkMap.put(8, "[] and Costs: Infinity");
+
+        assertThat(distanceMap).isEqualTo(checkMap);
+
+    }
+}
