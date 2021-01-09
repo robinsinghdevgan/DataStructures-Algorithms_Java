74fb0f437e83427b18cdf6630645ace0dbfbaf47
Robin Singh <robinsinghdevgan@outlook.com>
2021-01-09 13:29:50 +0530

    addded floyd warshal

---
+++ src/main/java/net/robinsinghdevgan/algorithms/Graphs/FloydWarshalAPSP.java
@@ -0,0 +1,84 @@
+package net.robinsinghdevgan.algorithms.Graphs;
+
+import java.util.HashMap;
+
+import net.robinsinghdevgan.dataStructures.Graphs.Graph;
+
+public class FloydWarshalAPSP<E extends Comparable<E>> {
+    public String allPairsShortestPath(Graph<E> graph) {
+        var costMatrix = new HashMap<E, HashMap<E, Double>>();
+        /*
+         * Initialize the cost matrix
+         */
+        var listOfVertices = graph.vertices();
+
+        for (var v : listOfVertices) {
+            costMatrix.put(v.getData(), new HashMap<E, Double>());
+            var costMapOfV = costMatrix.get(v.getData());
+            for (var w : listOfVertices)
+                costMapOfV.put(w.getData(), graph.getCost(v.getData(), w.getData()));
+
+        }
+
+        /*
+         * Add all vertices one by one to the set of intermediate vertices. ---> Before
+         * start of an iteration, we have shortest distances between all pairs of
+         * vertices such that the shortest distances consider only the vertices in set
+         * {0, 1, 2, .. k-1} as intermediate vertices. ----> After the end of an
+         * iteration, vertex no. k is added to the set of intermediate vertices and the
+         * set becomes {0, 1, 2, .. k}
+         */
+        for (var K : listOfVertices) {
+            // Pick all vertices as source one by one
+            var k = K.getData();
+            for (var I : listOfVertices) {
+                // Pick all vertices as destination for the
+                // above picked source
+                var i = I.getData();
+                for (var J : listOfVertices) {
+                    // If vertex k is on the shortest path from
+                    // i to j, then update the value of dist[i][j]
+                    var j = J.getData();
+                    if (costMatrix.get(i).get(k) + costMatrix.get(k).get(j) < costMatrix.get(i).get(j))
+                        costMatrix.get(i).put(j, costMatrix.get(i).get(k) + costMatrix.get(k).get(j));
+                }
+            }
+        }
+
+        // Identify negative cycles by propagating the value 'NEGATIVE_INFINITY'
+        // to every edge that is part of or reaches into a negative cycle.
+        for (var K : listOfVertices) {
+            // Pick all vertices as source one by one
+            var k = K.getData();
+            for (var I : listOfVertices) {
+                // Pick all vertices as destination for the
+                // above picked source
+                var i = I.getData();
+                for (var J : listOfVertices) {
+                    // If vertex k is on the shortest path from
+                    // i to j, then update the value of dist[i][j]
+                    var j = J.getData();
+                    if (costMatrix.get(i).get(k) + costMatrix.get(k).get(j) < costMatrix.get(i).get(j))
+                        costMatrix.get(i).put(j, Double.NEGATIVE_INFINITY);
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (var row : costMatrix.entrySet()) {
+            sb.append(row.getKey().toString() + " => | ");
+           
+            for (var col : row.getValue().entrySet()) {
+                var s = col.getValue().toString();
+                if(s == "Infinity")
+                    sb.append(" Inf" + " | ");
+                else if (s== "-Infinity")
+                    sb.append("-Inf" + " | ");
+                else
+                    sb.append(" " + s + " | ");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+}
---
+++ src/test/java/net/robinsinghdevgan/algorithms/Graphs/FloydWarshalAPSPTest.java
@@ -0,0 +1,62 @@
+package net.robinsinghdevgan.algorithms.Graphs;
+
+//import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import net.robinsinghdevgan.dataStructures.Graphs.Graph;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class FloydWarshalAPSPTest {
+    private Graph<Integer> createGraphWithOnlyPositiveEdgeCosts() {
+        var graph = new Graph<Integer>();
+        for (int i = 0; i < 9; i++) {
+            graph.addVertex(i);
+        }
+        graph.addEdge(0, 1, 4.0);
+        graph.addEdge(0, 7, 8.0);
+        graph.addEdge(1, 2, 8.0);
+        graph.addEdge(1, 7, 1.0);
+        graph.addEdge(2, 3, 7.0);
+        graph.addEdge(2, 8, 2.0);
+        graph.addEdge(2, 5, 4.0);
+        graph.addEdge(3, 4, 9.0);
+        graph.addEdge(3, 5, 1.0);
+        graph.addEdge(4, 5, 1.0);
+        graph.addEdge(5, 6, 2.0);
+        graph.addEdge(6, 7, 1.0);
+        graph.addEdge(6, 8, 6.0);
+        graph.addEdge(7, 8, 7.0);
+        return graph;
+    }
+
+    private Graph<Integer> createGraphWithNegativeEdgeCosts() {
+        var graph = new Graph<Integer>();
+        for (int i = 0; i < 9; i++) {
+            graph.addVertex(i);
+        }
+        graph.addEdge(0, 1, 1.0);
+        graph.addEdge(1, 2, 1.0);
+        graph.addEdge(2, 4, 1.0);
+        graph.addEdge(4, 3, -3.0);
+        graph.addEdge(3, 2, 1.0);
+        graph.addEdge(1, 5, 4.0);
+        graph.addEdge(1, 6, 4.0);
+        graph.addEdge(5, 6, 5.0);
+        graph.addEdge(6, 7, 4.0);
+        graph.addEdge(5, 7, 3.0);
+        return graph;
+    }
+
+    @Test
+    public void test1() {
+        var obj = new FloydWarshalAPSP<Integer>();
+        var graph = createGraphWithOnlyPositiveEdgeCosts();
+        var res = obj.allPairsShortestPath(graph);
+        System.out.println(res);
+        graph = createGraphWithNegativeEdgeCosts();
+        res = obj.allPairsShortestPath(graph);
+        System.out.println(res);
+    }
+}
