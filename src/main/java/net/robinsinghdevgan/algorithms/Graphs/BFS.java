66dea1f37a7252e644145d96b1e7a85153bbcc27
Robin Singh <robinsinghdevgan@outlook.com>
2021-01-09 18:20:25 +0530

    +BFS

---
+++ src/main/java/net/robinsinghdevgan/algorithms/Graphs/BFS.java
@@ -0,0 +1,46 @@
+package net.robinsinghdevgan.algorithms.Graphs;
+
+import java.util.HashSet;
+
+import net.robinsinghdevgan.dataStructures.QueueUsingSinglyLinkedList;
+import net.robinsinghdevgan.dataStructures.Graphs.Graph;
+
+public class BFS<E extends Comparable<E>> {
+    // returns cost to search target from source
+    public Double search(Graph<E> graph, E source, E target) {
+        if (source == target)
+            return 0.0;
+
+        var visited = new HashSet<E>();
+
+        var q = new QueueUsingSinglyLinkedList<E>();
+        q.offer(source);
+
+        Double cost = 0.0;
+        boolean found = false;
+        StringBuilder sb = new StringBuilder();
+        sb.append("Path: ");
+        while (!q.isEmpty()) {
+            System.out.println(q.toString());
+            E vertexValue = q.poll();
+            if (visited.contains(vertexValue))
+                continue;
+            visited.add(vertexValue);
+
+            if (vertexValue == target) {
+                sb.append(target + "\n");
+                found = true;
+                break;
+            }
+            var v = graph.getVertex(vertexValue);
+            sb.append(vertexValue + " -> ");
+
+            for (var e : v.getOutDegreeEdges()) {
+                cost += e.getCost();
+                q.offer(e.getTo());
+            }
+        }
+        System.out.println(sb.toString());
+        return found ? cost : Double.NEGATIVE_INFINITY;
+    }
+}
---
+++ src/test/java/net/robinsinghdevgan/algorithms/Graphs/BFSTest.java
@@ -0,0 +1,42 @@
+package net.robinsinghdevgan.algorithms.Graphs;
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import net.robinsinghdevgan.dataStructures.Graphs.Graph;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class BFSTest {
+    private Graph<Integer> createGraph() {
+        var graph = new Graph<Integer>();
+        for (int i = 0; i < 9; i++) {
+            graph.addVertex(i);
+        }
+        graph.addEdge(0, 1, 4.0);
+        graph.addEdge(0, 7, 8.0);
+        graph.addEdge(1, 2, 8.0);
+        graph.addEdge(1, 7, 1.0);
+        graph.addEdge(2, 3, 7.0);
+        graph.addEdge(2, 8, 2.0);
+        graph.addEdge(2, 5, 4.0);
+        graph.addEdge(3, 4, 9.0);
+        graph.addEdge(3, 5, 1.0);
+        graph.addEdge(4, 5, 1.0);
+        graph.addEdge(5, 6, 2.0);
+        graph.addEdge(6, 7, 1.0);
+        graph.addEdge(6, 8, 6.0);
+        graph.addEdge(7, 8, 7.0);
+        return graph;
+    }
+
+    @Test
+    public void test1() {
+        var graph = createGraph();
+        System.out.println(graph.toString());
+        var obj = new BFS<Integer>();
+        var cost = obj.search(graph, 0, 8);
+        assertThat(cost).isEqualTo(41.0);
+        System.out.println(cost);
+    }
+}
