e6ef1d4d9875e1dd69e51fa10bb29f4b48b8ba58
Robin Singh <robinsinghdevgan@outlook.com>
2021-01-09 21:25:26 +0530

    fixed bug in cost

--- src/main/java/net/robinsinghdevgan/algorithms/Graphs/BFS.java
+++ src/main/java/net/robinsinghdevgan/algorithms/Graphs/BFS.java
@@ -14,6 +14,8 @@ public class BFS<E extends Comparable<E>> {
         var visited = new HashSet<E>();
 
         var q = new QueueUsingSinglyLinkedList<E>();
+        var costQ = new QueueUsingSinglyLinkedList<Double>();
+        costQ.offer(0.0);
         q.offer(source);
 
         Double cost = 0.0;
@@ -22,6 +24,9 @@ public class BFS<E extends Comparable<E>> {
         sb.append("Path: ");
         while (!q.isEmpty()) {
             System.out.println(q.toString());
+
+            cost += costQ.poll();
+
             E vertexValue = q.poll();
             if (visited.contains(vertexValue))
                 continue;
@@ -36,7 +41,7 @@ public class BFS<E extends Comparable<E>> {
             sb.append(vertexValue + " -> ");
 
             for (var e : v.getOutDegreeEdges()) {
-                cost += e.getCost();
+                costQ.offer(e.getCost());
                 q.offer(e.getTo());
             }
         }
