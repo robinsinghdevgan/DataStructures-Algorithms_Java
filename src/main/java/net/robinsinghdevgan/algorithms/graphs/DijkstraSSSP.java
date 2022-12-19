f5a29c8edd7cdf5f17aabea6675a04f6dbb574bd
b0266172 <robin.singhdevgan@airtel.com>
2022-12-19 09:08:52 +0530

    Added explanatory comments to DijkstraSSSP.java

--- src/main/java/net/robinsinghdevgan/algorithms/graphs/DijkstraSSSP.java
+++ src/main/java/net/robinsinghdevgan/algorithms/graphs/DijkstraSSSP.java
@@ -4,33 +4,42 @@ import net.robinsinghdevgan.dataStructures.graphs.Graph;
 
 import java.util.*;
 
-public class DijkstraSSSP<Vertex extends Comparable<Vertex>> {
+public class DijkstraSSSP<V extends Comparable<V>> {
 
     private List<String> reconstructPath(
-            HashMap<Vertex, Double> distance,
-            HashMap<Vertex, Vertex> prev,
-            Vertex dest
+            HashMap<V, Double> distance,
+            HashMap<V, V> mapOfConnections,
+            V dest
     ) {
         List<String> path = new ArrayList<>();
+        /* if the distance to reach the destination vertex is 0,
+            or in other words, the 'dest' supplied is the 'source' parameter to 'getShortestPath' method,
+            return empty path
+         */
         if (distance.get(dest) == 0.0) return path;
-        Vertex at = prev.get(dest);
-        while (at != null) {
-            path.add(at.toString());
-            at = prev.get(at);
+
+        //get the node which is connected to the destination
+        V connectedVertex = mapOfConnections.get(dest);
+        while (connectedVertex != null) {
+            //Add this connected vertex to path
+            path.add(connectedVertex.toString());
+            //get the node which is connected to this vertex
+            connectedVertex = mapOfConnections.get(connectedVertex);
         }
+        //Reverse the path list, to get directions from start to destination
         Collections.reverse(path);
         return path;
     }
 
     // DijkstraSingleSourceShortestPath
-    public HashMap<Vertex, String> getShortestPath(Graph<Vertex> graph, Vertex source) {
+    public Map<V, String> getShortestPath(Graph<V> graph, V source) {
         //Let V be the number of vertices
-        int V = graph.numberOfVertices();
+        int numberOfVertices = graph.numberOfVertices();
 
         /* The priority-queue */
         //Let 'pq' be the priority queue of total size 'V', and is of type [source, destination-cost]
         var pq = new PriorityQueue<>(
-                V,
+                numberOfVertices,
                 new DistanceToVertexFromSource()
         );
         //The distance to source is 0.0, add this to queue
@@ -38,7 +47,7 @@ public class DijkstraSSSP<Vertex extends Comparable<Vertex>> {
 
         /* The distance-map */
         //Create a map to store distance to each vertex from the source
-        var mapOfDistanceToVertex = new HashMap<Vertex, Double>();
+        var mapOfDistanceToVertex = new HashMap<V, Double>();
         //The distance of source to source is 0.0
         mapOfDistanceToVertex.put(source, 0.0);
         //In the beginning, the distance to every vertex apart from the source is unknown, therefore POSITIVE_INFINITY
@@ -46,13 +55,13 @@ public class DijkstraSSSP<Vertex extends Comparable<Vertex>> {
             mapOfDistanceToVertex.put(vertex.getData(), Double.POSITIVE_INFINITY);
         }
 
-        var mapOfConnections = new HashMap<Vertex, Vertex>();
+        var mapOfConnections = new HashMap<V, V>();
 
         while (!pq.isEmpty()) {
             DistanceToVertexFromSource pqe = pq.poll();
-            Vertex vertexValue = pqe.source;
+            V vertexValue = pqe.source;
             Double distOfv = pqe.cost;
-            net.robinsinghdevgan.dataStructures.graphs.Vertex<Vertex> v = graph.getVertex(
+            net.robinsinghdevgan.dataStructures.graphs.Vertex<V> v = graph.getVertex(
                     vertexValue
             );
             var listOfEdges = v.getOutDegreeEdges();
@@ -73,23 +82,26 @@ public class DijkstraSSSP<Vertex extends Comparable<Vertex>> {
                 }
             }
         }
-        var result = new HashMap<Vertex, String>();
-        for (var dist : mapOfDistanceToVertex.entrySet()) {
+        //Build and return the result
+        var result = new HashMap<V, String>();
+        for (var entry : mapOfDistanceToVertex.entrySet()) {
+            V vertexToReach = entry.getKey();
+            Double costToReachTheVertex = entry.getValue();
             result.put(
-                    dist.getKey(),
-                    reconstructPath(mapOfDistanceToVertex, mapOfConnections, dist.getKey()) +
+                    entry.getKey(),
+                    reconstructPath(mapOfDistanceToVertex, mapOfConnections, vertexToReach) +
                             " and Costs: " +
-                            dist.getValue().toString()
+                            costToReachTheVertex
             );
         }
         return result;
     }
 
     class DistanceToVertexFromSource implements Comparator<DistanceToVertexFromSource> {
-        Vertex source;
+        V source;
         Double cost;
 
-        public DistanceToVertexFromSource(Vertex source, double d) {
+        public DistanceToVertexFromSource(V source, double d) {
             this.source = source;
             this.cost = d;
         }
@@ -100,6 +112,7 @@ public class DijkstraSSSP<Vertex extends Comparable<Vertex>> {
 
         @Override
         public int compare(DistanceToVertexFromSource o1, DistanceToVertexFromSource o2) {
+            //return -1 if o1 < o2, 0 if o1==o2 and 1 if o1 > o2
             return o1.cost.compareTo(o2.cost);
         }
     }
